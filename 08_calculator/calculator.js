e50233bcc062875f09440bdfbed425c732ee3e29
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-07-06 22:30:18 +0200

    Finish Excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sumed = array.reduce((a,b) => a+b ,0);
+  return sumed;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const sumed = array.reduce((a,b) => a*b ,1);
+  return sumed;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1
+  for(i=1; i<=a; i++){
+    sum *= i;
+  }
+	return sum;
 };
 
 // Do not edit below this line
