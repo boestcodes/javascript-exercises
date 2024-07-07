6734041de7911ac18bc4aea97e9c8959097b3028
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-07-07 22:31:16 +0200

    Finish Excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(numRaw) {
+    const num = parseInt(numRaw)
+    if (num == 0){
+        return 0;
+    } else if(num<0){
+        return "OOPS";
+    } 
+    
+    else {
+        let fibo = [1,1];
+        for(i=1; i<num; i++){
+            fibo.push(fibo[i-1]+fibo[i])
+        }
+        return fibo[num-1]
+    }
+    
 
 };
 
