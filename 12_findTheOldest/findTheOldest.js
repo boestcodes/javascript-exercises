e8d0878f07657179ac8253993f3147e3519bdf7e
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-07-10 21:40:01 +0200

    Optimise final solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,23 +1,14 @@
 const findTheOldest = function(persons) {
-    let person = {
-        name: "",
-        yearOfBirth: 0,
-        yearOfDeath: 0,
-    }
+    let person = { name: "", yearOfBirth: 0, yearOfDeath: 0,}
 
-    const date = new Date()
-    const today = date.getFullYear()
+    const today = new Date().getFullYear()
     let ageOldest = person.yearOfDeath - person.yearOfBirth;
     
     const oldestPerson = persons.reduce((acc, curr) => {
         let age = 0;
 
-        if('yearOfDeath' in curr){
-            age = curr.yearOfDeath - curr.yearOfBirth
-            
-        } else {
-            age = today - curr.yearOfBirth;
-        }
+        if('yearOfDeath' in curr) age = curr.yearOfDeath - curr.yearOfBirth;
+        else age = today - curr.yearOfBirth;
 
         if (age > ageOldest){
             person = curr
@@ -28,10 +19,7 @@ const findTheOldest = function(persons) {
         return acc
 
     }, {} )
-    
     return oldestPerson
-    
-    
 };
 
 // Do not edit below this line
